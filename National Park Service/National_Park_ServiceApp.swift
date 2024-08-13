892db910159f9960e749ef2498678d3dec8016c3
Lorenzo Pantano <l.pantano97@gmail.com>
2024-08-13 17:54:15 +0200

    Initial Commit

---
+++ National Park Service/National_Park_ServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_Park_ServiceApp.swift
+//  National Park Service
+//
+//  Created by Lorenzo Pantano on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct National_Park_ServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
